a286de9317b802d0784054efe8aa24058bcaed6e
DanielDonato <daniel_donato10@yahoo.com.br>
2018-11-11 20:15:25 -0200

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/danieldonato/workshopmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.danieldonato.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.danieldonato.workshopmongo.domain.User;
+import com.danieldonato.workshopmongo.reposiroty.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Gray", "bob@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
