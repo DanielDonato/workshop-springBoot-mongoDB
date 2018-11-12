e9992dc2c84fa114f1c97db112b5d9ebf2e96fc0
DanielDonato <daniel_donato10@yahoo.com.br>
2018-11-12 08:52:14 -0200

    Obtendo um usuario por id

---
+++ src/main/java/com/danieldonato/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.danieldonato.workshopmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.danieldonato.workshopmongo.service.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+}
--- src/main/java/com/danieldonato/workshopmongo/service/UserService.java
+++ src/main/java/com/danieldonato/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.danieldonato.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.danieldonato.workshopmongo.domain.User;
 import com.danieldonato.workshopmongo.reposiroty.UserRepository;
+import com.danieldonato.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
-	}	
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
 }
