ab7875ab68a2ab192e36dd5975888cd71d2380df
DanielDonato <daniel_donato10@yahoo.com.br>
2018-11-12 12:50:22 -0200

    Consulta com varios criterios

--- src/main/java/com/danieldonato/workshopmongo/reposiroty/PostRepository.java
+++ src/main/java/com/danieldonato/workshopmongo/reposiroty/PostRepository.java
@@ -1,5 +1,6 @@
 package com.danieldonato.workshopmongo.reposiroty;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,4 +14,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> seatchTitle(String text); //?0 -> primeiro parametro do metodo
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+	
 }
